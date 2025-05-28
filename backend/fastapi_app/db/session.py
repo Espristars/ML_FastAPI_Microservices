3fb0d7871a91821a125986a4b9e586624df6ca3e
Demid <110777080+Espristars@users.noreply.github.com>
2025-05-28 11:25:23 +0300

    Update session.py

--- backend/fastapi_app/db/session.py
+++ backend/fastapi_app/db/session.py
@@ -1,7 +1,17 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import sessionmaker
+from .models import Base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+
+
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+async def get_session():
+    async with SessionLocal() as session:
+        yield session
