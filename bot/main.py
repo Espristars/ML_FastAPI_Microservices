2595a6f0ed5074f6f5d4e30194735e413c9cf968
Demid <110777080+Espristars@users.noreply.github.com>
2025-05-28 11:30:02 +0300

    Update main.py

---
+++ bot/main.py
@@ -0,0 +1,23 @@
+import os
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from dotenv import load_dotenv
+
+from .handlers import router
+from .kafka_consumer import start_consumer_loop
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+dp.include_router(router)
+
+async def main():
+    start_consumer_loop()
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
